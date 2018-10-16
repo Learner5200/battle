0d514d1ebd6f0c293c5b44055424e0fd2c9021df
Chris Cooksley <christopher.cooksley@gmail.com>
2018-10-16 16:10:31 +0100

    cosmetic changes

--- app.rb
+++ app.rb
@@ -3,7 +3,13 @@ require "shotgun"
 
 class Battle < Sinatra::Base
   get '/' do
-    "Testing infrastructure working!"
+    erb(:index)
+  end
+
+  post '/play' do
+    @player_one = params[:player_one]
+    @player_two = params[:player_two]
+    erb(:play)
   end
 
   run! if app_file == $0
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,7 +1,15 @@
-feature "Testing infrastructure working!" do
-  scenario "should have 'Testing infrastructure working!'" do
-    visit '/'
-    expect(page).to have_content "Testing infrastructure working!"
+feature "names form" do
+  scenario "there is a form for users to enter their name" do
+    visit "/"
+    expect(page).to have_field("player_one")
+    expect(page).to have_field("player_two")
   end
 
+  scenario "submitted names should appear on screen" do
+    visit "/"
+    fill_in(:player_one, with: "Imtiyaz")
+    fill_in(:player_two, with: "Chris")
+    click_button('submit')
+    expect(page).to have_content("Imtiyaz vs Chris")
+  end
 end
