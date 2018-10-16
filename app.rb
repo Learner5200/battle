386e8a7830c6fdff723aee45288ff02552f148b2
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-16 16:38:05 +0100

    added a POST redirect

--- app.rb
+++ app.rb
@@ -2,15 +2,22 @@ require "sinatra/base"
 require "shotgun"
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
-  post '/play' do
-    @player_one = params[:player_one]
-    @player_two = params[:player_two]
-    erb(:play)
+  post '/names' do
+    session[:player_one] = params[:player_one]
+    session[:player_two] = params[:player_two]
+    redirect to('/play')
   end
 
+  get '/play' do
+    @player_one = session[:player_one]
+    @player_two = session[:player_two]
+    erb(:play)
+  end
   run! if app_file == $0
 end
